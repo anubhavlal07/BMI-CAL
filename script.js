5eedb9a3318711f75f2eb86213d80147d355c3bc
ANUBHAV LAL <anubhavlal.15@gmail.com>
2022-05-11 21:09:27 +0530

    Maintenance website

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
-var age = document.getElementById("age");
-var height = document.getElementById("height");
-var weight = document.getElementById("weight");
-var male = document.getElementById("m");
-var female = document.getElementById("f");
-var form = document.getElementById("form");
-function validateForm() {
-  if (
-    age.value == "" ||
-    height.value == "" ||
-    weight.value == "" ||
-    (male.checked == false && female.checked == false)
-  ) {
-    alert("All fields are required!");
-    document.getElementById("submit").removeEventListener("click", countBmi);
-  } else {
-    countBmi();
-  }
-}
-document.getElementById("submit").addEventListener("click", validateForm);
-function countBmi() {
-  var p = [height.value,age.value, weight.value];
-  if (male.checked) {
-    p.push("male");
-  } else if (female.checked) {
-    p.push("female");
-  }
-  // form.reset();
-  var bmi = Number(p[1]) / (((Number(p[2]) / 100) * Number(p[2])) / 100);
-  var result = "";
-  if (bmi < 18.5) {
-    result = "Underweight";
-  } else if (18.5 <= bmi && bmi <= 24.9) {
-    result = "Healthy";
-  } else if (25 <= bmi && bmi <= 29.9) {
-    result = "Overweight";
-  } else {
-    result = "Obese";
-  }
-  var h1 = document.createElement("h1");
-  var h2 = document.createElement("h2");
-  var res = document.createTextNode(result);
-  var b = document.createTextNode("BMI: ");
-  var value = document.createTextNode(parseFloat(bmi).toFixed(2));
-  h1.appendChild(res);
-  h2.appendChild(b);
-  h2.appendChild(value);
-  document.body.appendChild(h1);
-  document.body.appendChild(h2);
-  document.getElementById("submit").removeEventListener("click", countBmi);
-  document.getElementById("submit").removeEventListener("click", validateForm);
+var countdowndate = new Date(
+    "Feb 28,2022 20:00:00"
+).getTime(); /*To Set the time of release*/
+var x = setInterval(function () {
+    var curr = new Date().getTime();
+    var rem = countdowndate - curr;
+    var days = Math.floor(rem / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((rem % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mins = Math.floor((rem % (1000 * 60 * 60)) / (1000 * 60));
+    var secs = Math.floor((rem % (1000 * 60)) / 1000);
+    document.getElementById(
+        "launch"
+    ).innerHTML = `&nbsp${days}d ${hours}h ${mins}m ${secs}s&nbsp`;
+    if (rem < 0) {
+        clearInterval(x);
+        document.getElementById("launch").innerHTML = "Date Expired";
+    }
+}, 1000);
 
-  function clearForm() {
-    location.reload();
-  }
-  setInterval(clearForm, 5000);
+/*To diable the input from the user*/
+document.addEventListener(
+    "keydown",
+    function () {
+        if (event.keyCode == 123) {
+            return false;
+        } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+            return false;
+        } else if (event.ctrlKey && event.keyCode == 85) {
+            return false;
+        }
+    },
+    false
+);
+
+if (document.addEventListener) {
+    document.addEventListener(
+        "contextmenu",
+        function (e) {
+            e.preventDefault();
+        },
+        false
+    );
+} else {
+    document.attachEvent("oncontextmenu", function () {
+        window.event.returnValue = false;
+    });
 }
-document.getElementById("submit").addEventListener("click", countBmi);
